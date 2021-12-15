2d589ce35527888910f10a2c876634d5e732bff8
daxtonparke <daxtonparke@gmail.com>
2021-12-15 15:08:26 -0700

    idk mane

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', function (req,res) {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
 const port = process.env.PORT || 4004
 
 app.listen(port, ()=> console.log(`welcome to port ${port} stay a while`))
